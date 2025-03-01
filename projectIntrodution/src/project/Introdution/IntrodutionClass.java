43836ee7c9f5e5e17bdc6da96bac26257995b419
Eduardo <Eduardo@DESKTOP-UPCIMM2>
2025-03-01 20:00:02 -0300

    Introdution Project Criation

---
+++ projectIntrodution/src/project/Introdution/IntrodutionClass.java
@@ -0,0 +1,11 @@
+package project.Introdution;
+
+public class IntrodutionClass {
+	/* Main é um metodo auto executavel em Java */
+	public static void main(String[] args) {
+
+		/* Escreve texto no Console */
+		System.out.println("Hello my future employeers!");
+	}
+
+}
